a58b1026791ef8a2702743fc4fa5b616f1150e88
Apple <apple@Apples-MacBook-Pro.local>
2024-11-01 23:09:29 -0700

    first commit

---
+++ src/main/java/com/appswallet/test/TestController.java
@@ -0,0 +1,14 @@
+package com.appswallet.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@GetMapping({"/","/home","/status"})
+	public String getStatus() {
+		return "It is working... yay";
+	}
+}
