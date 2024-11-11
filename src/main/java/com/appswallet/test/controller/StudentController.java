af82379472b6dabd75881cd0a23d35fb817f1da0
Apple <apple@Apples-MacBook-Pro.local>
2024-11-11 08:39:29 -0800

    fixed

--- src/main/java/com/appswallet/test/controller/StudentController.java
+++ src/main/java/com/appswallet/test/controller/StudentController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appswallet.test.dao.StudentDao;
 import com.appswallet.test.model.Student;
 
 @RestController
+@RequestMapping("/street")
 public class StudentController {
 	
 	
