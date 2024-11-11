921c52c7607fb7262199558916e77463f0d62bbe
Apple <apple@Apples-MacBook-Pro.local>
2024-11-11 09:04:30 -0800

    slash adde

--- src/main/java/com/appswallet/test/controller/TestController.java
+++ src/main/java/com/appswallet/test/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appswallet.test.dao.EmpDao;
@@ -16,6 +17,7 @@ import com.appswallet.test.model.Employee;
 
 
 @RestController
+@RequestMapping("/")
 public class TestController {
 
 	
